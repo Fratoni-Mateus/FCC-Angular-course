87956a072c4e33c77b5e2509a872f1d2608db55d
Mateus Fratoni <Mateus.Fratonisouza@gmail.com>
2022-10-16 18:50:15 -0300

    Angular config basics and component creation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,21 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'hinv-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  // template: '<h1>Hello World from inline template!</h1>', //Example of an inline html template
+  //Example of multiline html template bellow
+  // template: `<h1>Hello World from multiline template!</h1>
+  //   <p>Angular is Awesome</p> `,
+  styleUrls: ['./app.component.scss'],
+  //Example of inline styling bellow
+  // styles: [
+  //   `
+  //     h1 {
+  //       color: red;
+  //     }
+  //   `,
+  // ],
 })
 export class AppComponent {
   title = 'hotelinventoryapp';
